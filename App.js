83ebe6fbafcbea3bb2a97b202a27dfb184aadadf
joenapper <joe.napper@hotmail.co.uk>
2021-07-28 23:57:52 +0100

    Switched uuidv4 for vanilla js approach

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, FlatList, StyleSheet} from 'react-native';
 import Header from './components/Header';
 
 const App = () => {
+  const [items, setItems] = useState([
+    {id: Math.random(), text: 'Milk'},
+    {id: Math.random(), text: 'Eggs'},
+    {id: Math.random(), text: 'Bread'},
+    {id: Math.random(), text: 'Juice'},
+  ]);
+
   return (
     <View style={styles.container}>
       <Header />
+      <FlatList
+        data={items}
+        renderItem={({item}) => <Text>{item.text}</Text>}
+      />
     </View>
   );
 };
